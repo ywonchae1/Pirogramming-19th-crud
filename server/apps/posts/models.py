83bf58acf4bebb37df5e504aab9a6115a44b3046
ywonchae1 <ywonchae62@gmail.com>
2023-07-13 11:37:20 +0900

    모델 생성

--- server/apps/posts/models.py
+++ server/apps/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    # title, user, content, region, price
+    title = models.CharField(max_length=64)
+    user = models.CharField(max_length=32)
+    content = models.TextField()
+    region = models.CharField(max_length=16)
+    price = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
